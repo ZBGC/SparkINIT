52e6585f45f18a327e9cd197fb75acc904af7514
ZBGC <105749803+ZBGC@users.noreply.github.com>
2023-04-14 16:21:24 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,62 +39,62 @@ export default function App() {
 
   return (
     <>
-    <div className="top-0 sticky" id="navBar">    
-    <nav className="border-gray-200 bg-[#081e3f] dark:border-gray-800 dark:border-gray-700" id="navBar">
-<div className=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
-    <a href="#" class="flex items-center ">
-        <img src={Logo} class="h-10 mr-3 " />
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-white ">JUMPIN-IT</span>
-    </a>
-    <div className="container flex hidden w-full md:block md:w-auto " id="navbar-solid-bg">
-        <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700 ">
-          <li className="">
-            <button 
-              className=" ml-5 block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent " aria-current="page"
-              onClick={handleAbout}
-            >
-              About
-            </button>
-          </li>
-          <li className="mr-2 sticky">
-          <button 
-              className=" block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent sticky" aria-current="page"
-              onClick={handleGallery}
-            >
-              Gallery
-            </button>
-          </li>
-          <li className="flex-1 mr-2 sticky">
-          <button 
-              className=" block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent sticky" aria-current="page"
-              onClick={handleDownload}
-            >
-              Download
-            </button>
-
-     <li className="mr-2 sticky">
-          <button 
-              className=" block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent sticky" aria-current="page"
-            >
-              Leaderboard
-            </button>
-          </li>
-
-            
-          </li>
-          <li class="flex-1 mr-2 sticky">
-          <button 
-              class=" block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent sticky" aria-current="page"
-              onClick={handleSoc}
-            >
-              Follow Us
-            </button>
-            
-          </li>
-        </ul>
-        </div>
-      </div>
-      </nav> </div>
+      <div className="top-0 sticky" id="navBar">    
+            <nav className="border-gray-200 bg-[#081e3f] dark:border-gray-800 dark:border-gray-700" id="navBar">
+        <div className=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
+            <a href="#" class="flex  items-center " onClick={() => navigate("/")}>
+                <img src={Logo} class="h-10 mr-3 " />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-white ">JUMPIN-IT</span>
+            </a>
+            <div className="container flex hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+                <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700 ">
+                    <li className="">
+                    <button 
+                        className=" block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent " aria-current="page"
+                        onClick={handleAbout}
+                    >
+                        About
+                    </button>
+                    </li>
+                    <li className="mr-2 sticky">
+                    <button 
+                        className=" block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent sticky" aria-current="page"
+                        onClick={handleGallery}
+                    >
+                        Gallery
+                    </button>
+                    </li>
+                    <li className="flex-1 mr-2 sticky">
+                    <button 
+                        className=" block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent sticky" aria-current="page"
+                        onClick={handleDownload}
+                    >
+                        Download
+                    </button>
+        
+                    </li>
+                    <li class="flex-1 mr-2 sticky">
+                    <button 
+                        onClick={() => navigate("/Leaderboard")}
+                        class=" block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent sticky"
+                        >
+                        Leaderboard
+                    </button>
+                    </li>
+                    <li class="flex-1 mr-2 sticky">
+                    <button 
+                        class=" block py-2 pl-5 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-white md:p-0 md:dark:text-white dark:bg-blue-600 md:dark:bg-transparent sticky" aria-current="page"
+                        onClick={handleSoc}
+                    >
+                        Follow Us
+                    </button>
+                    
+                    </li>
+                </ul>
+                </div>
+                </div>
+                </nav> 
+                </div>
 
       <div className="App">
 
